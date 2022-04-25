3f5860a3860b1b5a2862f7f410685d6830795d95
raadhosieni <raadhosieni@yahoo.com>
2022-04-25 18:49:28 +0300

    initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Webcam from "react-webcam";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Webcam />
+    </div>
+  );
+};
+
+export default App;
